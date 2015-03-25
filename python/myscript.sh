7f9b416604e04315161de5a06c26bc837f7ae94c
michareichmann <micha.reichmann@gmail.com>
2015-03-25 10:14:54 +0000

    Delete myscript.sh

--- python/myscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PXARDIR="$(readlink -f $(dirname $0)/..)"
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PXARDIR/lib
-export PYTHONPATH=$PYTHONPATH:$PXARDIR/lib
-
-# Run the example script using python and pxar core lib:
-python $PXARDIR/python/myscript.py $*                                           
