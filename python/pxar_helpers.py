e40c22bfb724a2090a26b2d1e7e6940f459109c0
Michael Reichmann <micha.reichmann@gmail.com>
2015-08-09 22:24:15 +0200

    cython changes to latest commit

--- python/pxar_helpers.py
+++ python/pxar_helpers.py
@@ -162,6 +162,7 @@ def PxarStartup(directory, verbosity):
         rocDacs.append(dacconfig.getAll())
         rocPixels.append(pixels)
 
+
     # set pgcal according to wbc
     pgcal = int(rocDacs[0]['wbc']) + 6
 
@@ -190,9 +191,9 @@ def PxarStartup(directory, verbosity):
     sig_delays = tbparameters.getAll()):
         print "WARNING: could not init DTB -- possible firmware mismatch."
         print "Please check if a new FW version is available"
-        exit
+        exit()
 
-
+    api.setDecodingOffset(config.get('decodingOffset'))
     print "And we have just initialized " + str(len(pixels)) + " pixel configs to be used for every ROC!"
 
     api.initDUT(int(config.get("hubId",31)),config.get("tbmType","tbm08"),tbmDACs,config.get("rocType"),rocDacs,rocPixels, rocI2C)
