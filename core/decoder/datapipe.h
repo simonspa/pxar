efc3b2ef2823bf00c86ee37db80d8e01a129774d
veloxid <bachmair@phys.ethz.ch>
2015-08-11 10:35:16 +0200

    fixed decoder for OnlineMon

--- core/decoder/datapipe.h
+++ core/decoder/datapipe.h
@@ -182,7 +182,7 @@ namespace pxar {
 public:
 
     dtbEventDecoder():  decodingStats(), readback(), eventID(-1), ultrablack(0xfff), black(0xfff),
-                        counter(0), sumB(0), sumUB(0), meanB(0), meanUB(0), offsetB(25) {};
+                        counter(0), sumB(0), sumUB(0), meanB(0), meanUB(0), offsetB(20) {};
     void Clear() { decodingStats.clear(); readback.clear(); count.clear(); shiftReg.clear(); eventID = -1; };
     void setOffset(uint8_t decodingOffset) { offsetB = decodingOffset; }
 
