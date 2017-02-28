f3071299fd7eba2d933761f1e04c4181e8da7d13
Michael Reichmann <micha.reichmann@gmail.com>
2017-02-28 19:28:50 +0100

    fix shell script to start the error finder for seeral voltages

--- python/all_errors.sh
+++ python/all_errors.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for v in 15 18 18
-do
-    for c in 3 3 6
-    do
-        echo 'Starting error finder for $v kV and $c mA'
-        python ErrorFinder.py -d $1 -v $v -c $c
-    done
+voltages=(15 18 18)
+currents=(3 3 6)
+for ((i=0;i<${#voltages[@]};++i)); do
+    v=${voltages[i]}
+    c=${currents[i]}
+    echo Starting error finder for ${v} kV and ${c} mA
+    python ErrorFinder.py -d $1 -vv ${v} -c ${c} -T 80
 done
