87a56fbc34c04c44b71acea48756b6fbc3cea33c
Michael Reichmann <micha.reichmann@gmail.com>
2017-02-28 19:30:03 +0100

    also fix plot map method for proc module

--- python/ErrorFinder.py
+++ python/ErrorFinder.py
@@ -11,11 +11,10 @@ from os.path import basename, dirname, realpath, split
 from os.path import join as joinpath
 from sys import argv, path
 from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
-from time import time, sleep
+from time import time
 from TreeWriter import TreeWriter
 lib_dir = joinpath(split(dirname(realpath(__file__)))[0], 'lib')
 path.insert(1, lib_dir)
-# path.insert(1, '/usr/local/bin')
 try:
     from id3003 import id3003_xray_generator
 except ImportError:
@@ -85,15 +84,18 @@ class ErrorFinder:
 
         # Find number of ROCs present:
         module = self.api.getNRocs() > 1
+        proc = 'proc' in self.api.getRocType()
         # Prepare new numpy matrix:
         d = zeros((417 if module else 52, 161 if module else 80))
         for px in data:
-            xoffset = 52 * (px.roc % 8) if module else 0
-            yoffset = 80 * int(px.roc / 8) if module else 0
+            roc = (px.roc - 12) % 16 if proc else 0
+            xoffset = 52 * (roc % 8) if module else 0
+            yoffset = 80 * int(roc / 8) if module else 0
+
             # Flip the ROCs upside down:
-            y = (px.row + yoffset) if (px.roc < 8) else (2 * yoffset - px.row - 1)
+            y = (px.row + yoffset) if (roc < 8) else (2 * yoffset - px.row - 1)
             # Reverse order of the upper ROC row:
-            x = (px.column + xoffset) if (px.roc < 8) else (415 - xoffset - px.column)
+            x = (px.column + xoffset) if (roc < 8) else (415 - xoffset - px.column)
             d[x][y] += 1 if count else px.value
 
         plot = Plotter.create_th2(d, 0, 417 if module else 52, 0, 161 if module else 80, name, 'pixels x', 'pixels y', name)
@@ -223,5 +225,5 @@ if __name__ == '__main__':
 
     # start command line
     z = ErrorFinder(args.dir, args.verbosity, args.trim, int(args.hv), int(args.cur))
-    #if args.hv is not None:
-    #    z.find_errors(5)
+    if args.hv is not None:
+        z.find_errors(5)
