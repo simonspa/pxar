74850f5a4eb36308ba1755d82bcf18533836be7d
Michael Reichmann <micha.reichmann@gmail.com>
2017-03-01 14:58:27 +0100

    updating xray settings list

--- python/all_errors.sh
+++ python/all_errors.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-voltages=(15 18 18)
-currents=(3 3 6)
+#voltages=(15 18 18 20 22 23 24 25 26 27)
+voltages=(29 31 33 35 37 39)
+#currents=(3 3 6 7 8 9 10 11 12 13)
+currents=(14 15 16 17 18 19)
 for ((i=0;i<${#voltages[@]};++i)); do
     v=${voltages[i]}
     c=${currents[i]}
