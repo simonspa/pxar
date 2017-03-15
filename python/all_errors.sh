d40e416e0981160ccfc0bd164b021559c2680159
Michael Reichmann <micha.reichmann@gmail.com>
2017-03-15 08:51:10 +0100

    defining new range for xray

--- python/all_errors.sh
+++ python/all_errors.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #voltages=(15 18 18 20 22 23 24 25 26 27 29 31 33 35 37 39)
-voltages=( $(for i in {41..60..2}; do echo ${i}; done) )
+voltages=( $(for i in {10..50..2}; do echo ${i}; done) )
 voltages[50]=60
 #currents=(3 3 6 7 8 9 10 11 12 13 14 15 16 17 18 19)
-currents=( $(for i in {21..30}; do echo ${i}; done) )
+currents=( $(for i in {5..25}; do echo ${i}; done) )
 for ((i=0;i<${#voltages[@]};++i)); do
     v=${voltages[i]}
     c=${currents[i]}
